91d9819667cf9adb0e44825f590987b23eac62ca
Kyle McGinley <kyleamcginley@gmail.com>
2022-09-06 17:04:04 -0400

    Subtraction works

--- myscript.js
+++ myscript.js
@@ -1,7 +1,17 @@
 function add(...args){
     let sum = 0;
     for (var i = 0; i < args.length; i++){
-        sum += args[i]
+        sum += args[i];
     }
     return sum;
 }
+
+function subtract(...args){
+    var difference = args[0];
+    for (var i = 1; i < args.length; i++){
+        difference -= args[i];
+    }
+    return difference;
+}
+
+console.log(subtract(5, 2))
