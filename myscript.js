8d804968064f9b220a9bec95ac67f3d245866187
Kyle McGinley <kyleamcginley@gmail.com>
2022-09-06 19:05:29 -0400

    Reworded first number functions
    Will copy and paste first number functions except
    have it equal second number

--- myscript.js
+++ myscript.js
@@ -48,81 +48,81 @@ const display = document.querySelector('#display');
 var firstNumber = '';
 
 const one = document.querySelector('#one');
-function addingOneToDisplay(){
+function addingOneToFirstNumber(){
     firstNumber += '1';
     display.textContent = firstNumber;
 }
-one.addEventListener('click', addingOneToDisplay);
+one.addEventListener('click', addingOneToFirstNumber);
 
 const two = document.querySelector('#two');
-function addingTwoToDisplay(){
+function addingTwoToFirstNumber(){
     firstNumber += '2';
     display.textContent = firstNumber;
 }
-two.addEventListener('click', addingTwoToDisplay);
+two.addEventListener('click', addingTwoToFirstNumber);
 
 const three = document.querySelector('#three');
-function addingThreeToDisplay(){
+function addingThreeToFirstNumber(){
     firstNumber += '3';
     display.textContent = firstNumber;
 }
-three.addEventListener('click', addingThreeToDisplay);
+three.addEventListener('click', addingThreeToFirstNumber);
 
 const four = document.querySelector('#four');
-function addingFourToDisplay(){
+function addingFourToFirstNumber(){
     firstNumber += '4';
     display.textContent = firstNumber;
 }
-four.addEventListener('click', addingFourToDisplay);
+four.addEventListener('click', addingFourToFirstNumber);
 
 const five = document.querySelector('#five');
-function addingFiveToDisplay(){
+function addingFiveToFirstNumber(){
     firstNumber += '5';
     display.textContent = firstNumber;
 }
-five.addEventListener('click', addingFiveToDisplay);
+five.addEventListener('click', addingFiveToFirstNumber);
 
 const six = document.querySelector('#six');
-function addingSixToDisplay(){
+function addingSixToFirstNumber(){
     firstNumber += '6';
     display.textContent = firstNumber;
 }
-six.addEventListener('click', addingSixToDisplay);
+six.addEventListener('click', addingSixToFirstNumber);
 
 const seven = document.querySelector('#seven');
-function addingSevenToDisplay(){
+function addingSevenToFirstNumber(){
     firstNumber += '7';
     display.textContent = firstNumber;
 }
-seven.addEventListener('click', addingSevenToDisplay);
+seven.addEventListener('click', addingSevenToFirstNumber);
 
 const eight = document.querySelector('#eight');
-function addingEightToDisplay(){
+function addingEightToFirstNumber(){
     firstNumber += '8';
     display.textContent = firstNumber;
 }
-eight.addEventListener('click', addingEightToDisplay);
+eight.addEventListener('click', addingEightToFirstNumber);
 
 const nine = document.querySelector('#nine');
-function addingNineToDisplay(){
+function addingNineToFirstNumber(){
     firstNumber += '9';
     display.textContent = firstNumber;
 }
-nine.addEventListener('click', addingNineToDisplay);
+nine.addEventListener('click', addingNineToFirstNumber);
 
 const zero = document.querySelector('#zero');
-function addingZeroToDisplay(){
+function addingZeroToFirstNumber(){
     firstNumber += '0';
     display.textContent = firstNumber;
 }
-zero.addEventListener('click', addingZeroToDisplay);
+zero.addEventListener('click', addingZeroToFirstNumber);
 
 const clear = document.querySelector('#clear');
-function clearingDisplay(){
+function clearToFirstNumber(){
     firstNumber = '';
     display.textContent = firstNumber;
 }
-clear.addEventListener('click', clearingDisplay);
+clear.addEventListener('click', clearToFirstNumber);
 
 //By now the firstNumber is entered into display
 //Need to store the new first number when an operator key is pressed
